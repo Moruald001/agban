4afffa38d4a5e2bef57c653f40374b0b8d70a4aa
romuald <sodouessodina@gmail.com>
2025-11-04 13:47:10 +0000

    test

--- agbn-APi/config/corsOption.js
+++ agbn-APi/config/corsOption.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 
 const corsOptions = {
   // origin: "http://localhost:5173", // ton front
-  origin: "*"
+  origin: "*",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
