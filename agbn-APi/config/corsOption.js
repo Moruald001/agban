f68637ee938e5b94972fdb6b7308bb3e818d9ec5
romuald <sodouessodina@gmail.com>
2025-11-04 13:07:29 +0000

    port allowed changed

--- agbn-APi/config/corsOption.js
+++ agbn-APi/config/corsOption.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "http://localhost:5173", // ton front
+  origin: "http://localhost:3000", // ton front
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
