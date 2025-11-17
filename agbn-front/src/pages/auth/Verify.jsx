4763e13be11552c7f931ca5841eca0d743adf35d
romuald <sodouessodina@gmail.com>
2025-11-17 19:50:11 +0000

    verify page done

--- agbn-front/src/pages/auth/Verify.jsx
+++ agbn-front/src/pages/auth/Verify.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import succesGif from "../../assets/icons8-success.gif";
 
 export default function Verify() {
   return (
-    <div>
-      <h1>Votre email à été vérifié avec succès</h1>
+    <div className="w-screen  h-screen flex flex-col justify-center items-center gap-3">
+      <img src={succesGif} alt="gif succès animé" />
+      <h1 className="text-2xl">Votre email à été vérifié avec succès</h1>
       <Link
         to={"/login"}
-        className="w-auto h-1.5 bg-gray-700 text-white hover:scale-125 transition-transform duration-300"
+        className=" bg-gray-600 p-3 rounded-lg opacity-80 text-white font-center  hover:scale-115 transition-transform duration-300  "
       >
         Se connecter
       </Link>
